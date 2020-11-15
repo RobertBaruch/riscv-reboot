59b11e8bd4d7fe0fa8b9a48c633b6ace153f05fc
Robert Baruch <robert.c.baruch@gmail.com>
2020-11-15 12:18:12 -0800

    Adds reg card and async memory

---
+++ async_memory.py
@@ -0,0 +1,182 @@
+# Disable pylint's "your name is too short" warning.
+# pylint: disable=C0103
+from typing import List, Tuple
+
+from nmigen import Array, Signal, Module, Elaboratable, ClockDomain
+from nmigen.build import Platform
+from nmigen.sim import Simulator, Delay
+from nmigen.asserts import Assert, Assume, Cover, Past, Stable, Rose, Fell, AnyConst, Initial
+
+from util import main
+
+
+class AsyncMemory(Elaboratable):
+    """Logic for a typical asynchronous memory.
+
+    Attributes:
+        addr: The address to read or write.
+        data_in: The input data (when writing).
+        data_out: The output data (when reading).
+        n_oe: Output enable, active low. When n_oe is 1, the output is 0.
+        n_wr: Write, active low.
+    """
+
+    addr: Signal
+    data_in: Signal
+    data_out: Signal
+    n_oe: Signal
+    n_wr: Signal
+
+    def __init__(self, width: int, addr_lines: int):
+        """Constructs an asynchronous memory.
+
+        Args:
+            width: The number of bits in each memory cell ("word").
+            addr_lines: The number of address lines. We only support memories
+                up to 16 address lines (so 64k words).
+        """
+        assert width > 0
+        assert addr_lines > 0
+        assert addr_lines <= 16
+
+        self.addr = Signal(addr_lines)
+        self.n_oe = Signal()
+        self.n_wr = Signal()
+
+        # There's no such thing as a tristate or bidirectional port
+        # in nMigen, so we do this instead.
+        self.data_in = Signal(width)
+        self.data_out = Signal(width)
+
+        # The actual memory
+        self._mem = Array(
+            [Signal(width, reset_less=True) for _ in range(2**addr_lines)])
+
+    def elaborate(self, _: Platform) -> Module:
+        """Implements the logic of an asynchronous memory.
+
+        Essentially implements the wr-controlled write cycle, where
+        the oe signal doesn't matter. We can't really implement the
+        delays involved, so be safe with your signals :)
+        """
+        m = Module()
+
+        # Local clock domain so we can clock data into the memory
+        # on the positive edge of n_wr.
+        wr_clk = ClockDomain("wr_clk", local=True)
+        m.domains.wr_clk = wr_clk
+        wr_clk.clk = self.n_wr
+
+        m.d.comb += self.data_out.eq(0)
+        with m.If(~self.n_oe & self.n_wr):
+            m.d.comb += self.data_out.eq(self._mem[self.addr])
+        m.d.wr_clk += self._mem[self.addr].eq(self.data_in)
+
+        return m
+
+    @classmethod
+    def sim(cls):
+        """A quick simulation of the async memory."""
+        m = Module()
+        m.submodules.mem = mem = AsyncMemory(width=32, addr_lines=5)
+
+        sim = Simulator(m)
+
+        def process():
+            yield mem.n_oe.eq(0)
+            yield mem.n_wr.eq(1)
+            yield mem.data_in.eq(0xFFFFFFFF)
+            yield Delay(1e-6)
+            yield mem.addr.eq(1)
+            yield mem.n_oe.eq(0)
+            yield Delay(1e-6)
+            yield mem.n_oe.eq(1)
+            yield Delay(1e-6)
+            yield mem.data_in.eq(0xAAAA1111)
+            yield Delay(1e-6)
+            yield mem.n_wr.eq(0)
+            yield Delay(0.2e-6)
+            yield mem.n_wr.eq(1)
+            yield Delay(0.2e-6)
+            yield mem.data_in.eq(0xFFFFFFFF)
+            yield mem.n_oe.eq(0)
+            yield Delay(1e-6)
+            yield mem.addr.eq(0)
+            yield Delay(1e-6)
+
+        sim.add_process(process)
+        with sim.write_vcd("async_memory.vcd"):
+            sim.run_until(10e-6)
+
+    @classmethod
+    def formal(cls) -> Tuple[Module, List[Signal]]:
+        """Formal verification for the async memory.
+
+        Note that you MUST have multiclock on in the sby file, because there is
+        more than one clock in the system -- the default formal clock and the
+        local clock inside the memory.
+        """
+        m = Module()
+        m.submodules.mem = mem = AsyncMemory(width=32, addr_lines=5)
+
+        # Assume "good practices":
+        # * n_oe and n_wr are never simultaneously 0, and any changes
+        #   are separated by at least a cycle to allow buffers to turn off.
+        # * memory address remains stable throughout a write cycle, and
+        #   is also stable just before a write cycle.
+
+        m.d.comb += Assume(mem.n_oe | mem.n_wr)
+
+        # Paren placement is very important! While Python logical operators
+        # and, or have lower precedence than ==, the bitwise operators
+        # &, |, ^ have *higher* precedence. It can be confusing when it looks
+        # like you're writing a boolean expression, but you're actually writing
+        # a bitwise expression.
+        with m.If(Fell(mem.n_oe)):
+            m.d.comb += Assume((mem.n_wr == 1) & (Past(mem.n_wr) == 1))
+        with m.If(Fell(mem.n_wr)):
+            m.d.comb += Assume((mem.n_oe == 1) & (Past(mem.n_oe) == 1))
+        with m.If(Rose(mem.n_wr) | (mem.n_wr == 0)):
+            m.d.comb += Assume(Stable(mem.addr))
+
+        m.d.comb += Cover((mem.data_out == 0xAAAAAAAA)
+                          & (Past(mem.data_out) == 0xBBBBBBBB))
+
+        # Make sure that when the output is disabled, the output is zero, and
+        # when enabled, it's whatever we're pointing at in memory.
+        with m.If(mem.n_oe == 1):
+            m.d.comb += Assert(mem.data_out == 0)
+        with m.Else():
+            m.d.comb += Assert(mem.data_out == mem._mem[mem.addr])
+
+        # If we just wrote data, make sure that cell that we pointed at
+        # for writing now contains the data we wrote.
+        with m.If(Rose(mem.n_wr)):
+            m.d.comb += Assert(mem._mem[Past(mem.addr)] == Past(mem.data_in))
+
+        # Pick an address, any address.
+        check_addr = AnyConst(5)
+
+        # We assert that unless that address is written, its data will not
+        # change. To know when we've written the data, we have to create
+        # a clock domain to let us save the data when written.
+
+        saved_data_clk = ClockDomain("saved_data_clk")
+        m.domains.saved_data_clk = saved_data_clk
+        saved_data_clk.clk = mem.n_wr
+
+        saved_data = Signal(32)
+        with m.If(mem.addr == check_addr):
+            m.d.saved_data_clk += saved_data.eq(mem.data_in)
+
+        with m.If(Initial()):
+            m.d.comb += Assume(saved_data == mem._mem[check_addr])
+            m.d.comb += Assume(mem.n_wr == 1)
+        with m.Else():
+            m.d.comb += Assert(saved_data == mem._mem[check_addr])
+
+        return m, [mem.addr, mem.data_in, mem.n_wr, mem.n_oe]
+
+
+if __name__ == "__main__":
+    main(AsyncMemory)
---
+++ reg_card.py
@@ -0,0 +1,321 @@
+# Disable pylint's "your name is too short" warning.
+# pylint: disable=C0103
+from typing import List, Tuple
+
+from nmigen import Signal, Module, Elaboratable, ClockDomain, ClockSignal
+from nmigen import Mux
+from nmigen.build import Platform
+from nmigen.asserts import Assert, Assume, Cover, Past, Stable, Rose, AnyConst, Initial
+
+from async_memory import AsyncMemory
+from transparent_latch import TransparentLatch
+from util import main
+
+
+class RegCard(Elaboratable):
+    """Logic for the register card.
+
+    Attributes:
+        data_x: The X bus, always written to.
+        data_y: The Y bus, always written to.
+        data_z: The Z bus, always read from.
+        reg_to_x: The control line indicating we want to output a register to X.
+        reg_to_y: The control line indicating we want to output a register to Y.
+        z_to_reg: The control line indicating we want to write a register from Z.
+        reg_x: The register to output to X.
+        reg_y: The register to output to Y.
+        reg_z: The register to write from Z.
+
+    The card optionally outputs the data in reg_x to the data_x bus,
+    and the data in reg_y to the data_y bus. It also optionally
+    writes the data on the data_z bus to reg_z.
+
+    This module uses two system-wide clocks: ph1 and ph2. The phases look
+    like this:
+
+           ________          ________          
+    ph1  _|   RD   |___WR___|   RD   |___WR___|
+         ___     ____     ____     ____     ___
+    ph2     |___|    |___|    |___|    |___|
+
+    ph1 controls whether we're reading or writing the memories, while
+    ph2 controls the read/write pulse (and memory input buffers) to the memories
+    if we are writing and latches if we are reading.
+
+    The phases also don't both change at the same time.
+
+    Strictly speaking that's 6 clocks per machine cycle.
+    """
+
+    data_x: Signal
+    data_y: Signal
+    data_z: Signal
+    reg_to_x: Signal
+    reg_to_y: Signal
+    z_to_reg: Signal
+    reg_x: Signal
+    reg_y: Signal
+    reg_z: Signal
+
+    def __init__(self):
+        """Constructs a register card."""
+        # Buses
+        self.data_x = Signal(32)
+        self.data_y = Signal(32)
+        self.data_z = Signal(32)
+
+        # Controls
+        self.reg_to_x = Signal()
+        self.reg_to_y = Signal()
+        self.z_to_reg = Signal()
+        self.reg_x = Signal(5)
+        self.reg_y = Signal(5)
+        self.reg_z = Signal(5)
+
+        # Submodules
+        self._x_bank = AsyncMemory(width=32, addr_lines=5)
+        self._y_bank = AsyncMemory(width=32, addr_lines=5)
+        self._x_latch = TransparentLatch(size=32)
+        self._y_latch = TransparentLatch(size=32)
+        self._x_bank_wr_latch = TransparentLatch(size=32)
+        self._y_bank_wr_latch = TransparentLatch(size=32)
+
+    def elaborate(self, _: Platform) -> Module:
+        """Implements the logic of the register card."""
+        m = Module()
+
+        ph1 = ClockSignal("ph1")
+        ph2 = ClockSignal("ph2")
+        x_bank = self._x_bank
+        y_bank = self._y_bank
+        x_latch = self._x_latch
+        y_latch = self._y_latch
+        x_bank_wr_latch = self._x_bank_wr_latch
+        y_bank_wr_latch = self._y_bank_wr_latch
+
+        m.submodules.x_bank = x_bank
+        m.submodules.y_bank = y_bank
+        m.submodules.x_latch = x_latch
+        m.submodules.y_latch = y_latch
+        m.submodules.x_bank_wr_latch = x_bank_wr_latch
+        m.submodules.y_bank_wr_latch = y_bank_wr_latch
+
+        # Some aliases
+        read_phase = ph1
+        write_phase = ~ph1
+        read_pulse = ph1 & ~ph2  # high pulse during read phase
+        write_pulse = ph1 | ph2  # low pulse during write phase
+
+        # Checks for register 0
+        x_reg_0 = Signal()
+        y_reg_0 = Signal()
+        m.d.comb += [
+            x_reg_0.eq(self.reg_x == 0),
+            y_reg_0.eq(self.reg_y == 0),
+        ]
+
+        # Write signals for both banks are tied together.
+        n_wr = Signal()
+        m.d.comb += [
+            x_bank.n_wr.eq(n_wr),
+            y_bank.n_wr.eq(n_wr),
+        ]
+
+        # If we want to write, then pass through the write pulse to
+        # the memory banks.
+        m.d.comb += n_wr.eq(1)
+        with m.If(self.z_to_reg):
+            m.d.comb += n_wr.eq(write_pulse)
+
+        # We also read from the memories on ph1, but only if we're
+        # not reading from register 0. Strictly speaking this doesn't
+        # matter, since if we are reading from register 0, the memory
+        # output is switched out by the output multiplexer.
+        x_bank_oe = ~x_reg_0 & read_phase
+        y_bank_oe = ~y_reg_0 & read_phase
+        m.d.comb += [
+            x_bank.n_oe.eq(~x_bank_oe),
+            y_bank.n_oe.eq(~y_bank_oe),
+        ]
+
+        # The address for each bank is switched between x/y during the
+        # read phase, and z during the write phase.
+        x_addr = Signal(5)
+        y_addr = Signal(5)
+        m.d.comb += [
+            x_addr.eq(Mux(read_phase, self.reg_x, self.reg_z)),
+            y_addr.eq(Mux(read_phase, self.reg_y, self.reg_z)),
+            x_bank.addr.eq(x_addr),
+            y_bank.addr.eq(y_addr),
+        ]
+
+        # The memory data is the data coming out of the memory, or the
+        # data coming out of the write latches. Since high-Z is simulated
+        # by outputting zeros, this is fine.
+        x_mem_data = Signal(32)
+        y_mem_data = Signal(32)
+        m.d.comb += [
+            x_mem_data.eq(x_bank.data_out | x_bank_wr_latch.data_out),
+            y_mem_data.eq(y_bank.data_out | y_bank_wr_latch.data_out),
+        ]
+
+        # The memory data inputs are the memory data.
+        m.d.comb += [
+            x_bank.data_in.eq(x_mem_data),
+            y_bank.data_in.eq(y_mem_data),
+        ]
+
+        # The write latches aren't actually used as latches, just
+        # tri-state buffers. They are always transparent. But, n_oe
+        # is used to control the output.
+        wr_latch_oe = ~n_wr
+        m.d.comb += [
+            x_bank_wr_latch.data_in.eq(self.data_z),
+            y_bank_wr_latch.data_in.eq(self.data_z),
+            x_bank_wr_latch.le.eq(1),
+            y_bank_wr_latch.le.eq(1),
+            x_bank_wr_latch.n_oe.eq(~wr_latch_oe),
+            y_bank_wr_latch.n_oe.eq(~wr_latch_oe),
+        ]
+
+        # The bus output latches take data from the memory data and
+        # optionally pass it to the x/y buses, transparently during
+        # the read cycle, but the data is latched so it remains during
+        # the write cycle. However, the output of the latches to the
+        # buses is controlled by the control signals.
+        #
+        # Also, multiplex between the memory data and zero depending
+        # on whether we're reading register 0.
+        x_zero_oe = x_reg_0 & read_phase
+        y_zero_oe = y_reg_0 & read_phase
+
+        m.d.comb += [
+            x_latch.data_in.eq(Mux(x_zero_oe, 0, x_mem_data)),
+            y_latch.data_in.eq(Mux(y_zero_oe, 0, y_mem_data)),
+            x_latch.le.eq(read_pulse),
+            y_latch.le.eq(read_pulse),
+            x_latch.n_oe.eq(~self.reg_to_x),
+            y_latch.n_oe.eq(~self.reg_to_y),
+        ]
+
+        # The x and y buses get the output latch outputs.
+        m.d.comb += [
+            self.data_x.eq(x_latch.data_out),
+            self.data_y.eq(y_latch.data_out),
+        ]
+
+        return m
+
+    @classmethod
+    def formal(cls) -> Tuple[Module, List[Signal]]:
+        """Formal verification for the register card."""
+        m = Module()
+
+        ph1 = ClockDomain("ph1")
+        ph2 = ClockDomain("ph2")
+        regs = RegCard()
+
+        m.domains += [ph1, ph2]
+        m.submodules += regs
+
+        # Generate the ph1 and ph2 clocks.
+        cycle_count = Signal(8, reset=0, reset_less=True)
+        phase_count = Signal(3, reset=0, reset_less=True)
+
+        m.d.sync += phase_count.eq(phase_count + 1)
+        with m.Switch(phase_count):
+            with m.Case(0, 1, 2):
+                m.d.comb += ph1.clk.eq(1)
+            with m.Default():
+                m.d.comb += ph1.clk.eq(0)
+        with m.Switch(phase_count):
+            with m.Case(1, 4):
+                m.d.comb += ph2.clk.eq(0)
+            with m.Default():
+                m.d.comb += ph2.clk.eq(1)
+        with m.If(phase_count == 5):
+            m.d.sync += phase_count.eq(0)
+            m.d.sync += cycle_count.eq(cycle_count + 1)
+
+        # This is how we expect to use the card.
+        with m.If(phase_count > 0):
+            m.d.comb += [
+                Assume(Stable(regs.reg_x)),
+                Assume(Stable(regs.reg_y)),
+                Assume(Stable(regs.reg_z)),
+                Assume(Stable(regs.reg_to_x)),
+                Assume(Stable(regs.reg_to_y)),
+                Assume(Stable(regs.z_to_reg)),
+                Assume(Stable(regs.data_z)),
+            ]
+
+        # Figure out how to get to the point where X and Y are nonzero and different.
+        m.d.comb += Cover((regs.data_x != 0) & (regs.data_y != 0)
+                          & (regs.data_x != regs.data_y))
+
+        # X and Y buses should not change during a cycle, except for the first phase
+        with m.Switch(phase_count):
+            with m.Case(2, 3, 4, 5):
+                with m.If(regs.data_x != 0):
+                    m.d.comb += Assert(Stable(regs.data_x))
+                with m.If(regs.data_y != 0):
+                    m.d.comb += Assert(Stable(regs.data_y))
+
+        # X and Y buses should be zero if there is no data transfer.
+        with m.If(regs.reg_to_x == 0):
+            m.d.comb += Assert(regs.data_x == 0)
+        with m.If(regs.reg_to_y == 0):
+            m.d.comb += Assert(regs.data_y == 0)
+
+        with m.If(phase_count > 0):
+            # X and Y buses should be zero if we read from register 0.
+            with m.If(regs.reg_to_x & (regs.reg_x == 0)):
+                m.d.comb += Assert(regs.data_x == 0)
+            with m.If(regs.reg_to_y & (regs.reg_y == 0)):
+                m.d.comb += Assert(regs.data_y == 0)
+
+        write_pulse = Signal()
+        m.d.comb += write_pulse.eq(phase_count != 4)
+
+        # On write, the data should have been written to both banks.
+        with m.If(Rose(write_pulse) & regs.z_to_reg):
+            m.d.comb += Assert(regs._x_bank._mem[Past(regs.reg_z)]
+                               == Past(regs.data_z))
+            m.d.comb += Assert(regs._y_bank._mem[Past(regs.reg_z)]
+                               == Past(regs.data_z))
+
+        # Pick an register, any register, except 0. We assert that unless
+        # it is written, its data will not change.
+
+        check_addr = AnyConst(5)
+        saved_data = Signal(32)
+        stored_x_data = Signal(32)
+        stored_y_data = Signal(32)
+
+        write_pulse_domain = ClockDomain("write_pulse_domain", local=True)
+        m.domains.write_pulse_domain = write_pulse_domain
+        write_pulse_domain.clk = write_pulse
+
+        m.d.comb += Assume(check_addr != 0)
+        m.d.comb += [
+            stored_x_data.eq(regs._x_bank._mem[check_addr]),
+            stored_y_data.eq(regs._y_bank._mem[check_addr]),
+        ]
+
+        with m.If(regs.z_to_reg & (regs.reg_z == check_addr)):
+            m.d.write_pulse_domain += saved_data.eq(regs.data_z)
+
+        with m.If(Initial()):
+            m.d.comb += Assume(saved_data == stored_x_data)
+            m.d.comb += Assume(stored_x_data == stored_y_data)
+        with m.Else():
+            m.d.comb += Assert(saved_data == stored_x_data)
+            m.d.comb += Assert(saved_data == stored_y_data)
+
+        return m, [regs.data_z, regs.reg_to_x, regs.reg_to_y, regs.z_to_reg,
+                   regs.reg_x, regs.reg_y, regs.reg_z, ph1.clk, ph2.clk, saved_data,
+                   stored_x_data, stored_y_data]
+
+
+if __name__ == "__main__":
+    main(RegCard)
